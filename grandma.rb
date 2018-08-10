4f7972f18e20b8cfcb77cae68cd6ee47a26b03fa
Gavin Soko <sokolofg@gmail.com>
2018-08-10 20:34:15 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 def speak_to_grandma(string)
-  if string != string.upcase 
+  if string === string.upcase 
     "HUH?! SPEAK UP, SONNY!"
   elseif string === "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
